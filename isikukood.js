d1e53166b1854116f78359ab27d352577b91d34e
gitTanzj <kalle.riit@gmail.com>
2023-09-22 11:18:44 +0300

    isikukood arvutus

--- isikukood.js
+++ isikukood.js
@@ -35,15 +35,42 @@ function isikukood(kood){
         day: kood.slice(5,7),
         qNumber : kood.slice(7,10),
         cNumber : kood.slice(-1)
-    }   
+    }
+    const myControl = (code) => {
+    const weight1 = [1,2,3,4,5,6,7,8,9,1]
+    const weight2 = [3,4,5,6,7,8,9,1,2,3]
+
+    // 49403136526
+
+    let sum = 0
+    for(let i = 0; i < code.length - 1; i++){
+        sum = sum + parseInt(code[i]) * weight1[i]
+    }
+    if(sum % 11 < 10){
+        return sum % 11
+    }
+    else{
+        let sum2 = 0
+        for(let i = 0; i < code.length -1; i++){
+            sum2 = sum2 + parseInt(code[i]) * weight2[i]
+        }
+        if(sum2 % 11 < 10){
+            return sum2 % 11
+        }
+        else{
+            return 0
+        }
+    }
+
+}
     const ans = [
         `Isik isikukoodiga ${kood} on ${data.sex},`,
         `kes on syndinud ${data.day}. ${months[data.month]}il ${getCentury(kood[0])}${data.year}. aastal.`,
         `Tema isikukood registreeriti`,
         `Ja sel paeval oli ta ${data.qNumber}. ${data.sex}`,
-        `Isikukoodi kontrollnumbriks on ${data.cNumber}`
+        `Isikukoodi kontrollnumbriks on ${myControl(kood)}`
     ]
     return ans
 }
 
-console.log(isikukood("50604292787"))
+console.log(isikukood("50608095716"))
